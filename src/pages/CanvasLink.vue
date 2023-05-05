78d4c5a30f201d456524b3fe5f8d82e35a0c6483
swc9803 <swc9803@gmail.com>
2023-05-05 17:41:57 +0900

    거리가 가까워지면 서로를 이어주는 선 생성

--- src/pages/CanvasLink.vue
+++ src/pages/CanvasLink.vue
@@ -9,19 +9,46 @@ import { ref, onMounted, onBeforeUnmount } from "vue";
 
 const canvasRef = ref();
 let ctx;
+let effect;
 
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.x = Math.random() * this.effect.width;
-    this.y = Math.random() * this.effect.height;
-    this.radius = 15;
+    this.radius = Math.random() * 6 + 3;
+    this.x =
+      this.radius + Math.random() * (this.effect.width - this.radius * 2);
+    this.y =
+      this.radius + Math.random() * (this.effect.height - this.radius * 2);
+    this.vx = Math.random() * 1 - 0.5;
+    this.vy = Math.random() * 1 - 0.5;
   }
   draw(context) {
-    context.fillStyle = `hsl(${this.x * 0.5}, 100%, 50%)`;
+    const gradient = context.createLinearGradient(
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height
+    );
+    gradient.addColorStop(0, "white");
+    gradient.addColorStop(0.5, "magenta");
+    gradient.addColorStop(1, "blue");
+
+    context.fillStyle = gradient;
+    context.fill();
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.fill();
+    context.strokeStyle = "white";
+    context.stroke();
+  }
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x > this.effect.width - this.radius || this.x < this.radius) {
+      this.vx *= -1;
+    }
+    if (this.y > this.effect.height - this.radius || this.y < this.radius) {
+      this.vy *= -1;
+    }
   }
 }
 
@@ -40,14 +67,38 @@ class Effect {
     }
   }
   handleParticles(context) {
+    this.connectParticles(context);
     this.particles.forEach((particle) => {
       particle.draw(context);
+      particle.update();
     });
+  }
+  connectParticles(context) {
+    const maxDistance = 100;
+    for (let a = 0; a < this.particles.length; a++) {
+      for (let b = a; b < this.particles.length; b++) {
+        const dx = this.particles[a].x - this.particles[b].x;
+        const dy = this.particles[a].y - this.particles[b].y;
+        const distance = Math.hypot(dx, dy);
+        if (distance < maxDistance) {
+          context.save();
+          const opacity = 1 - distance / maxDistance;
+          context.globalAlpha = opacity;
+          context.beginPath();
+          context.moveTo(this.particles[a].x, this.particles[a].y);
+          context.lineTo(this.particles[b].x, this.particles[b].y);
+          context.stroke();
+          context.restore();
+        }
+      }
+    }
   }
 }
 
 function animate() {
-  //   requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height);
+  effect.handleParticles(ctx);
+  requestAnimationFrame(animate);
 }
 
 const onResize = () => {
@@ -59,8 +110,7 @@ onMounted(() => {
   ctx = canvasRef.value.getContext("2d");
   onResize();
 
-  const effect = new Effect(canvasRef.value);
-  effect.handleParticles(ctx);
+  effect = new Effect(canvasRef.value);
 
   animate();
   window.addEventListener("resize", onResize);
