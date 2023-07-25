4a8d8d1f2936b1b0e9f2c5dcbb9a1d4735155ef3
swc9803 <swc9803@gmail.com>
2023-07-25 18:25:10 +0900

    galaga 발사체 구현

--- src/pages/Galaga.vue
+++ src/pages/Galaga.vue
@@ -18,23 +18,64 @@ class Player {
     this.height = 100;
     this.x = this.game.width * 0.5 - this.width * 0.5;
     this.y = this.game.height - this.height;
-    this.speed = 10;
+    this.speed = 5;
   }
   draw(context) {
     context.fillRect(this.x, this.y, this.width, this.height);
   }
   update() {
-    if (this.game.keys.indexOf("ArrowLeft") > -1) this.x -= this.speed;
-    if (this.game.keys.indexOf("ArrowRight") > -1) this.x += this.speed;
+    if (this.game.keys.indexOf("ArrowLeft") > -1) this.x -= this.speed; // && a
+    if (this.game.keys.indexOf("ArrowRight") > -1) this.x += this.speed; // && d
 
-    if (this.x < 0) this.x = 0;
-    else if (this.x > this.game.width - this.width) {
-      this.x = this.game.width - this.width;
+    // canvas 범위 이탈 방지
+    // if (this.x < 0) this.x = 0;
+    // else if (this.x > this.game.width - this.width) {
+    //   this.x = this.game.width - this.width;
+    // }
+    if (this.x < -this.width * 0.5) this.x = -this.width * 0.5;
+    else if (this.x > this.game.width - this.width * 0.5) {
+      this.x = this.game.width - this.width * 0.5;
+    }
+  }
+  shoot() {
+    const projectile = this.game.getProjectile();
+    if (projectile) {
+      projectile.start(this.x + this.width * 0.5, this.y);
     }
   }
 }
 
-// class Projectile {}
+class Projectile {
+  constructor() {
+    this.width = 8;
+    this.height = 40;
+    this.x = 0;
+    this.y = 0;
+    this.speed = 10;
+    this.free = true;
+  }
+  draw(context) {
+    if (!this.free) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+  update() {
+    if (!this.free) {
+      this.y -= this.speed;
+      if (this.y < 0 - this.height) {
+        this.reset();
+      }
+    }
+  }
+  start(x, y) {
+    this.x = x - this.width + 0.5;
+    this.y = y;
+    this.free = false;
+  }
+  reset() {
+    this.free = true;
+  }
+}
 
 // class Enemy {}
 
@@ -46,23 +87,38 @@ class Game {
     this.keys = [];
     this.player = new Player(this);
 
+    this.projectilesPool = [];
+    this.numberOfProjectiles = 10;
+    this.createProjectiles();
+
     window.addEventListener("keydown", (e) => {
-      if (this.keys.indexOf(e.key) === -1) {
-        this.keys.push(e.key);
-      }
-      console.log(this.keys);
+      if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+      if (e.key === "1") this.player.shoot();
     });
     window.addEventListener("keyup", (e) => {
       const index = this.keys.indexOf(e.key);
-      if (index > -1) {
-        this.keys.splice(index, 1);
-      }
-      console.log(this.keys);
+      if (index > -1) this.keys.splice(index, 1);
     });
   }
   render(context) {
     this.player.draw(context);
     this.player.update();
+    this.projectilesPool.forEach((projectile) => {
+      projectile.update();
+      projectile.draw(context);
+    });
+  }
+  createProjectiles() {
+    for (let i = 0; i < this.numberOfProjectiles; i++) {
+      this.projectilesPool.push(new Projectile());
+    }
+  }
+  getProjectile() {
+    for (let i = 0; i < this.projectilesPool.length; i++) {
+      if (this.projectilesPool[i].free) {
+        return this.projectilesPool[i];
+      }
+    }
   }
 }
 
